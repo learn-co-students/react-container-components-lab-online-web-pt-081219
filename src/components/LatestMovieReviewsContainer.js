093f52c55cd573503e5a8716127f3a65afd894b3
James <jamescscalise@gmail.com>
2020-05-17 20:04:00 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,30 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+
+   constructor(props){
+       super(props)
+        this.state = {
+            reviews: []
+        }
+   }
+   
+   componentDidMount(){
+    fetch('https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=q0gUhgiqL06sOWe1N4ZnQBCur9f6f4Bz')
+    .then(res => res.json())
+    .then(
+        reviewData =>  this.setState({reviews: reviewData.results})
+    )
+   }
+
+  render() {
+    return (
+      <div className="latest-movie-reviews">
+          <MovieReviews reviews = {this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -7,3 +7,53 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+    constructor(props){
+        super(props)
+         this.state = {
+             reviews: [],
+             searchTerm: ''
+         }
+    }
+
+    loadValues(term){
+        fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=q0gUhgiqL06sOWe1N4ZnQBCur9f6f4Bz&query=${term}`)
+        .then(res => res.json())
+        .then(
+            reviewData =>  this.setState({reviews: reviewData.results})
+        )
+       }
+
+    handleChange = event => {
+         this.setState({
+             searchTerm: event.target.value
+        })
+
+    }
+
+     handleSubmit = event => {
+        alert('A name was submitted: ' + this.state.searchTerm)
+        console.log(event)
+        event.preventDefault()
+        this.loadValues(this.state.searchTerm)
+      }
+
+    render() {
+      return (
+        <div className="searchable-movie-reviews">
+            <form onSubmit={this.handleSubmit}>
+                 <label>
+                 Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+             </form>
+             <MovieReviews reviews = {this.state.reviews} />
+
+        </div>
+      );
+    }
+  }
+  
+  export default SearchableMovieReviewsContainer;

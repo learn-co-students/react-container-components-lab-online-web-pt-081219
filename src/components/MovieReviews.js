7396b2a2525af6ef5a4b2353d5be0b25626aa93c
github username <github email address>
2020-05-18 23:01:51 -0600

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReview = ({ reviews }) => (
+    
+    <div className="review-list">
+        {reviews.map(review => <div key={review.id} className="review"><h3> {review.display_title} </h3></div> )}
+    </div>
+
+)
+
+export default MovieReview
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,7 +3,50 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            searchTerm: '',
+            reviews: []
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    queryList = (event) => {
+        const URL = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}&`
+            + `api-key=${NYT_API_KEY}`;
+        event.preventDefault()
+
+        fetch(URL)
+            .then(response => response.json())
+            .then(reviewData => this.setState({ reviews: reviewData.results }))
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={event => this.queryList(event)}>
+                    <input id="query" name="query" type="text" onChange={event => this.handleChange(event)} value={this.state.searchTerm} />
+                <button value="submit">Submit</button>
+                </form>
+            <div className="searchable-movie-reviews">
+                {/* {console.log(this.state.reviews)} */}
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer;
